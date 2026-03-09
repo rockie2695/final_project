770ef5cc8a5a166a206b33a7957248e545d94743
“Aidan <aidanchoi@arpainfinity.com>
2026-03-09 18:00:58 +0800

    fix: task 6

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 app = Flask("Emotion Detector")
-
 
 @app.route("/emotionDetector")
 def sent_detector():
-    text_to_analyze = request.args.get("textToAnalyze")
+    text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-
-    return response
-
+    
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, 'sadness': {}. The dominant emotion is {}.".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
